5a115e6df9cf46673caf48470022c825bbef5320
장현애 <aeae_1@naver.com>
2022-03-22 16:28:14 +0900

    section 4: basic grammar

--- querydsl/querydsl/src/test/java/study/querydsl/entity/MemberTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/entity/MemberTest.java
@@ -3,14 +3,11 @@ package study.querydsl.entity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
